19313094d39b197c013f44715ff3d7512f3979d2
budRich <robstenklippa@gmail.com>
2023-04-01 07:48:56 +0000

    support inline comments
    previously only lines where the first nonblank character was "#"
    was considered a comment.
    
    Now it is possible to have a line like this: chromium vivaldi # firefox
    where " firefox" would be a comment and therefor ignored.

--- func/parse_packages.sh
+++ func/parse_packages.sh
@@ -4,5 +4,10 @@ parse_packages() {
   {
     [[ -f "$dir_config/packages" ]] && cat "$dir_config/packages"
     packages.d_is_not_empty && cat "$dir_config/packages.d/"*
-  } | sed -r 's/(^\s*|\s*$)//g;/^(#|$)/d;s/\s+/\n/g' | sort -u > "$dir_tmp/sorted"
+  } | sed -r '
+        s/(^\s*|\s*$)//g;
+        /^(#|$)/d;
+        s/\s*#.+$//g;
+        s/\s+/\n/g
+      ' | sort -u > "$dir_tmp/sorted"
 }
